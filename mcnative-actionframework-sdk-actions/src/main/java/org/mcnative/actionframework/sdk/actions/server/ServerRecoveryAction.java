49c302301882c45c3e6ee7c0d69e30d3891ca0af
Fridious <philippfriedhoff@outlook.de>
2021-01-16 18:11:51 +0100

    Added ServerRecoveryAction

---
+++ mcnative-actionframework-sdk-actions/src/main/java/org/mcnative/actionframework/sdk/actions/server/ServerRecoveryAction.java
@@ -0,0 +1,45 @@
+package org.mcnative.actionframework.sdk.actions.server;
+
+import io.netty.buffer.ByteBuf;
+import org.mcnative.actionframework.sdk.common.action.MAFAction;
+import org.mcnative.actionframework.sdk.common.protocol.codec.BufferUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ServerRecoveryAction implements MAFAction {
+
+    public static final String NAMESPACE = "srv";
+    public static final String NAME = "info";
+
+    private Map<UUID, Integer> onlinePlayers;
+
+    @Override
+    public String getNamespace() {
+        return NAMESPACE;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public void read(int version, ByteBuf buffer) {
+        int size = buffer.readInt();
+        this.onlinePlayers = new HashMap<>(size);
+        for (int i = 0; i < size; i++) {
+            this.onlinePlayers.put(BufferUtil.readUniqueId(buffer), buffer.readInt());
+        }
+    }
+
+    @Override
+    public void write(ByteBuf buffer) {
+        buffer.writeInt(this.onlinePlayers.size());
+        for (Map.Entry<UUID, Integer> entry : onlinePlayers.entrySet()) {
+            BufferUtil.writeUniqueId(buffer, entry.getKey());
+            buffer.writeInt(entry.getValue());
+        }
+    }
+}
